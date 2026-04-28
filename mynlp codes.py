be53e6df6413fb00e452ca63a7e37e1880170f11
Geethaa73 <geethaa1911@gmail.com>
2026-04-28 11:08:12 -0700

    Add NLP techniques: lemmatization, POS tagging, and more

--- mynlp codes.py
+++ mynlp codes.py
@@ -38,3 +38,87 @@ stemmed_word=[stemmer.stem(word) for word in words]
 print("stemmed words:",stemmed_words)
 
 
+#lemmatisation
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+nltk.download("wordnet")
+lemmatizer = WordNetLemmatizer()
+words = ["running","eating","best","fairly"]
+lemmatized_words = [lemmatizer.lemmatize(word,pos=wordnet.VERB)for word in words]
+print("lemmatized_words:",lemmatized_words)
+
+#parts fo speech tagging
+
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+nltk.download("punkt")
+nltk.download("averaged_perceptron_tagger")
+nltk.download("averaged_perceptron_tagger_eng")
+sentence = "The quick brown fox jumps over the lazy dog."
+tokens= word_tokenize(sentence)
+tagged=pos_tag(tokens)
+print(tagged)
+
+#Sytax and parsing
+exp = "5+3*3"
+result=eval(exp)
+print(result)
+
+#Lowercasing
+text = "Hello World"
+lowercase_text = text.lower()
+print(lowercase_text)
+
+#remove a special characters
+import re
+def remove(text):
+    return re.sub(r'[^A-Za-z0-9\s]',"",text)
+i = "hello!@@#$$& world"
+c = remove(i)
+print(c)
+
+#Remove punctuation
+
+import string
+def remove_punctuation(text):
+    return text.translate(str.maketrans('', '', string.punctuation))
+# Example usage
+text = "Hello, world! Let's remove punctuation."
+clean_text = remove_punctuation(text)
+print(clean_text)
+
+
+
+#Bag of words
+
+from sklearn.feature_extraction.text import CountVectorizer
+# Sample documents
+documents = [
+"I love programming in Python",
+"Python is great for data science",
+"I love learning new programming languages"
+]
+# Create the Bag of Words model
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(documents)
+# Get feature names and transformed data
+feature_names = vectorizer.get_feature_names_out()
+print("Feature Names:", feature_names)
+print("Bag of Words Model:\n", X.toarray())
+
+
+
+#n-grams
+
+def n_grams(text, n):
+    words = text.split()
+    return [tuple(words[i:i + n]) for i in range(len(words) - n + 1)]
+# Example usage
+text = "I love programming in Python"
+n = 2
+print(n_grams(text, n))
+
+
